dea082c7454112009e61006d8b0dd324fa8889cd
Subhamtyagi684 <subhamtyagi85@gmail.com>
2021-05-20 17:51:18 +0530

    message will receive only /a and /a not /b or /c in url

--- chatapp/consumers.py
+++ chatapp/consumers.py
@@ -1,5 +1,48 @@
 from channels.consumer import SyncConsumer,AsyncConsumer
 from asgiref.sync import async_to_sync
+import json
+
+class ChatConsumer(SyncConsumer):
+
+    def websocket_connect(self,event):
+        self.room_name = self.scope['url_route']['kwargs']['uid']
+        # Join room group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_name,
+            self.channel_name
+        )
+
+        self.send({
+            'type':'websocket.accept'
+        })
+
+    def websocket_disconnect(self,event):
+        # Leave room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_name,
+            self.channel_name
+        )
+    
+    def websocket_receive(self,event):
+        # Send message to room group
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_name,
+            {
+                'type': 'websocket_message',
+                'message': event['text']
+            }
+        )
+
+    def websocket_message(self,event):
+
+        message = event['message']
+        self.send({
+            'type':'websocket.send',
+            'text':message
+        })
+
+
+
 
 class EchoConsumer(SyncConsumer):
 
--- chatproject/asgi.py
+++ chatproject/asgi.py
@@ -13,7 +13,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter,URLRouter
 from django.urls import path
 from channels.auth import AuthMiddlewareStack
-from chatapp.consumers import EchoConsumer
+from chatapp.consumers import EchoConsumer,ChatConsumer
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatproject.settings')
 
@@ -21,7 +21,8 @@ application = ProtocolTypeRouter({
         'http':get_asgi_application(),
         'websocket':AuthMiddlewareStack( 
             URLRouter([
-                path('ws/chat/',EchoConsumer.as_asgi())
+                path('ws/chat/',EchoConsumer.as_asgi()),
+                path('ws/chat/<str:uid>/',ChatConsumer.as_asgi())
             ])
         )
      })
